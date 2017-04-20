21f02b48e81e887bd53da43f8fa7d785539d86e0
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-04-19 19:57:21 -0700

    Updates in-progress

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './app.css';
+import './app.css';
 import logo from './imgs/logo.svg';
 
 const App = () => (
     <div>
-        <div className={styles.app}>
-            <img src={logo} className={styles.rotate}/>
+        <div className="app">
+            <img src={logo} className="rotate"/>
             <h1>Welcome to React!</h1>
         </div>
     </div>
