ef7d6f8b3b870f26056dd0cca9a937d3b1584ef1
Scott Bowler <scott.bowler@learningfuze.com>
2017-11-10 10:58:04 -0800

    Removed October theme

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import './app.css';
+import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
-import reactPumpkin from '../assets/images/october-react-logo-glow.png';
-import webs from '../assets/images/web-corner.png';
-import spider from '../assets/images/spider.png';
 
 const App = () => (
     <div>
         <div className="app">
-            <img src={reactPumpkin} className="logo rotate"/>
+            <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
-            <img src={webs} className="webs-r"/>
-            <img src={webs} className="webs-l"/>
-            <img src={spider} className="spider shake"/>
         </div>
     </div>
 );
