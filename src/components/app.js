42765db2851bb7f38e51edcbdd69332176edc359
Scott Bowler <scott.bowler@learningfuze.com>
2017-12-28 20:56:33 -0800

    Fixed push to wrong repo

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,11 @@ import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 
 const App = () => (
-    <div className="app">
-        <h1>Memory Match</h1>
+    <div>
+        <div className="app">
+            <img src={logo} className="logo rotate"/>
+            <h1>Welcome to React</h1>
+        </div>
     </div>
 );
 
