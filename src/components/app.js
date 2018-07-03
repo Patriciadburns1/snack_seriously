28388da7ec998341e94ff6adda733b2ba0502603
vokamura <vcokamura@gmail.com>
2018-07-03 11:46:37 -0700

    added to App

--- src/components/app.js
+++ src/components/app.js
@@ -9,9 +9,9 @@ import Filters from './filters';
 
 const App = () => (
         <div>
-            <Filters />
-            {/* <Nav/> */}
-            {/* <Route exact path='/' component= {Homepage}/> */}
+            {/* <Filters /> */}
+            <Nav/>
+            <Route exact path='/' component= {Homepage}/>
             {/* <Route path='/search/:term?' component= {Homepage}/> */}
             {/* <Route path='/search:term/MultipleResults/:ids?' component={MultipleResults}/> DONT ERASE FOR WHEN SERVER IS UP  */}
             {/* <Route path='/MultipleResults' component={MultipleResults}/>
