268bc500074997ce6b209ad3d5977bdc27c1888c
James Wang <jamesnewking@gmail.com>
2018-07-05 11:40:10 -0700

    added getcategory php file

---
+++ public/api/getcategory.php
@@ -0,0 +1,41 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+require_once('../../server/config/snack_db.php');
+$conn = mysqli_connect($db_cred['host'],$db_cred['user'],$db_cred['password'],$db_cred['database']);
+$query = "SELECT `p`.`ID`, `p`.`name`, `d`.`img_url` FROM `products` AS `p` JOIN `details` AS `d` ON `d`.`product_id` = `p`.`ID` WHERE `p`.`category_id` = 1 ORDER BY `p`.`ID` ASC LIMIT 12 OFFSET 0";
+
+//print("query = $query");
+
+$result = mysqli_query($conn, $query);
+
+$output = [
+    'success' => false,
+    'data' => [],
+    'error' => []
+];
+
+if ($result){
+    //the query successfully ran
+    if(mysqli_num_rows($result)>0 ){
+        //there was data
+        while( $row = mysqli_fetch_assoc($result) ) {
+            //get all the data
+            $output['data'][] = $row;
+        }
+        $output['success'] = true;
+        }
+        else{
+            //there was no data
+            $output['error'][] = 'no data avail';
+        }
+
+
+}else{
+    //the query failed
+    $output['error'][] = mysqli_error($conn);
+}
+
+$output_json = json_encode($output);
+
+print($output_json);
+?>
