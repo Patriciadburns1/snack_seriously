462732e6b47b7e9cd71b887c055d816a69e29ba2
dburns1 <patriciadburns@gmail.com>
2018-07-10 18:53:45 -0700

    fixed div

--- src/components/singleresultpage/singleresult.js
+++ src/components/singleresultpage/singleresult.js
@@ -6,30 +6,30 @@ import Ingredient from './ingredient';
 import SingleItem from './singleitem'
 import Search from '../searchbar';
 import axios from 'axios';
-import NotFound from './notfound'; 
+import NotFound from './notfound';
 
 class SingleResult extends Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             userInput: '',
             singleItem: null,
-            error: false ,
+            error: false,
             loading: true
         }
-        this.handleInputChange=this.handleInputChange.bind(this); 
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
-  
+
     componentDidMount() {
         this.getSingleResult();
     }
-    
+
     handleInputChange = (event) => {
         const value = event.target.value;
         this.setState({
             userInput: value,
-        })  
-        this.props.history.push('/search/' + value);   
+        })
+        this.props.history.push('/search/' + value);
     }
 
     async getSingleResult() {
@@ -39,48 +39,50 @@ class SingleResult extends Component {
         this.setState({
             singleItem: response.data,
             loading: false
-        }, ()=>{
-            if(!this.state.singleItem.success){
+        }, () => {
+            if (!this.state.singleItem.success) {
                 this.setState({
-                    singleItem:null, 
-                    error: true 
+                    singleItem: null,
+                    error: true
                 })
             }
         });
-     }
-    
+    }
+
 
     render() {
-        const {singleItem, error, loading} = this.state;
+        const { singleItem, error, loading } = this.state;
 
-        if(!loading && error){
-            return(
-                <div> <NotFound/> </div> 
+        if (!loading && error) {
+            return (
+                <div> <NotFound /> </div>
             )
         }
 
-        if(loading && !singleItem || !singleItem.success){
+        if (loading && !singleItem || !singleItem.success) {
             return <h1>Loading...</h1>;
-        } else {
-            const {nutrients, ingredients, img_url, name, manu, per_container, size, unit, weight}=this.state.singleItem.data; 
+        } 
+        else {
+            const { nutrients, ingredients, img_url, name, manu, per_container, size, unit, weight } = this.state.singleItem.data;
             console.log(ingredients);
             const params = this.props.match.params.term || '';
             return (
+                <div>
                     <div>
-                        <div>
-                            <div className="searchBarComp">
-                                <input type="text" placeholder="Search snacks" onChange={this.handleInputChange}/>
-                                <Link to = {`/MultipleResults/${params}`}> <span className="icon"> <i>&#x1F50D;</i> </span> </Link> 
-                            </div>
+                        <div className="searchBarComp">
+                            <input type="text" placeholder="Search snacks" onChange={this.handleInputChange} />
+                            <Link to={`/MultipleResults/${params}`}> <span className="icon"> <i>&#x1F50D;</i> </span> </Link>
                         </div>
-                    <div className="singleResultContainer">
-                <SingleItem name={name} manu={manu} img={img_url}/>
-                 <Nutritionalfacts nutrition={nutrients} per_container={per_container} size={size} unit={unit} weight={weight}/>
-                <Ingredient ingredients={ingredients} />
                     </div>
-                    )
+                    <div className="singleResultContainer">
+                        <SingleItem name={name} manu={manu} img={img_url} />
+                        <Nutritionalfacts nutrition={nutrients} per_container={per_container} size={size} unit={unit} weight={weight} />
+                        <Ingredient ingredients={ingredients} />
+                    </div>
+                </div>
+            )
         }
     }
 }
-    
+
 export default SingleResult;
