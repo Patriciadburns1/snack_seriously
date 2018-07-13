6f313fb02572d05661a13271f9090720e63a46f8
Jeff Lieu <jefflieu@gmail.com>
2018-07-13 10:26:42 -0700

    committing changes

--- src/components/about.js
+++ src/components/about.js
@@ -6,8 +6,22 @@ import '../assets/css/about.css';
 export default () =>{
     return(
         <div>
-            <Link to="/About"><div className="about"></div></Link>
-            <h1>About us...Coming Soon</h1>
+            <Link to="/About">
+                <div className="about">
+                    We are SNACKING SERIOUSLY!
+                </div>
+            </Link>
+            <h1 className="snacktitle">Snackers</h1>
+
+            <div className="grid-container">
+                <div className="item1">Vikki Okamura</div>
+                <div className="item2">Patricia Burns</div>
+                <div className="item3">Peter Nguyen</div>
+                <div className="item4">James Wang</div>
+                <div className="item5">Jeff Lieu</div>
+                <div className="item6">Lou Pruitt</div>
+            </div>
+
         </div>
 
     )
