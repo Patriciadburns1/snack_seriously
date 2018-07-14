52befbc3da7a2ee16a322d4a97a00d3c84d73329
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-07-14 16:14:47 -0700

    usersignup printname

--- public/api/apiActionCalls/usersignup.php
+++ public/api/apiActionCalls/usersignup.php
@@ -18,28 +18,28 @@ if($result){
     }
     print( json_encode($output));
     exit();
-}
-
-$query = "INSERT INTO `users`(`ID`, `email`, `password`, `username`, `user_rights`) 
-VALUES ( 'null', $_EMAIL, $_PASS, $_NAME, 1)";
-
-$result = mysqli_query($conn, $query);
-
-if($result){
-    if(mysqli_num_rows($result)>0){
-        $userData = mysqli_fetch_assoc($result);//we only expect 1 row
-        $output['name'] = $userData['name'];
-        $output['success'] = true;
-        $_SESSION['userID'] = $userData['ID'];
-        $_SESSION['username'] = $userData['username'];
-    }else{
-        $output['error'] = 'invalid user name or password';
-    }
 }else{
-    $output['error'] = 'cannot attempt login';
-    $output['msg1'] = $_NAME;
-    $output['msg1'] = $_PASS;
-    $output['msg1'] = $_EMAIL;
-}
 
+    $query = "INSERT INTO `users`(`ID`, `email`, `password`, `username`, `user_rights`) 
+    VALUES ( 'null', $_EMAIL, $_PASS, $_NAME, 1)";
+
+    $result = mysqli_query($conn, $query);
+
+    if($result){
+        if(mysqli_num_rows($result)>0){
+            $userData = mysqli_fetch_assoc($result);//we only expect 1 row
+            $output['name'] = $userData['name'];
+            $output['success'] = true;
+            $_SESSION['userID'] = $userData['ID'];
+            $_SESSION['username'] = $userData['username'];
+        }else{
+            $output['error'] = 'invalid user name or password';
+        }
+    }else{
+        $output['error'] = 'cannot attempt login';
+        $output['msg1'] = $_NAME;
+        $output['msg2'] = $_PASS;
+        $output['msg3'] = $_EMAIL;
+    }
+}
 print( json_encode($output));
