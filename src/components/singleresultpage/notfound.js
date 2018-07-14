2973d87f095cc953671539d26273cb0273e8f1b1
Jeff Lieu <jefflieu@gmail.com>
2018-07-14 16:16:27 -0700

    styled not found page

--- src/components/singleresultpage/notfound.js
+++ src/components/singleresultpage/notfound.js
@@ -1,13 +1,17 @@
 import React from 'react'; 
 import '../../assets/css/homepage.css';
 import { Link } from 'react-router-dom';
-
+import snacker from '../../assets/images/notfound/snacker.gif';
+import '../../assets/css/notfound.css';
 
 export default ()=> {
     return(
-        <div> 
-        <div className="pageNotFound"> Page not found </div> 
-            <Link to='/'> <div className="returnHomePage"> Click here to return to homepage </div> </Link> 
+        <div className="pageNotFoundContainer">
+        <div className="pageNotFound">
+            <h3>Page not found</h3>
+        </div>
+            <img className="snacker" src={snacker} />
+            <Link to='/'> <div className="returnHomePage"> Click here to return to homepage </div> </Link>
         </div> 
     )
 }
