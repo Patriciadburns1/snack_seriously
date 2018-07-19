1e0ebe9e99d44b8ee1b68301781337f64edf6c40
James Wang <jamesnewking@gmail.com>
2018-07-19 10:39:38 -0700

    add new user login authentication for frontend

--- src/components/createaccount.js
+++ src/components/createaccount.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/contact.css';
-import axios from 'axios'; 
+//import axios from 'axios';
 import { SearchDataContext } from './searchdata';
 
 
@@ -9,19 +9,85 @@ class CreateAccount extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            userCheck:{
+                msg: "4-15 characters",
+                msgClass: "regularMsg",
+                valid: false
+            },
+            emailCheck:{
+                msg: "valid email address",
+                msgClass: "regularMsg",
+                valid: false
+            },
+            passwordCheck:{
+                msg: "at least 1 letter, 1 number, and at least 8 characters",
+                msgClass: "regularMsg",
+                valid: false
+            },
+            submitMsg: "",
             form:{
                 username: '',
                 email:'',
                 password: ''
             }
-           
-        }
+        };
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleInputChange(event){    
-        const {value, name} = event.target; 
-        //console.log("name", name, "value", value); 
+    handleInputChange(event){
+        const regexUser = /^[a-z0-9_-]{3,15}$/;
+        const regexEmail = /(.+)@(.+){2,}\.(.+){2,}/;
+        const regexPassword = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/;
+
+        if(regexUser.test( this.state.form.username)){
+            this.setState({
+                userCheck : {msg : "valid username",
+                            msgClass : "regularMsg",
+                            valid: true
+                }
+            }, this.checkValid);
+        } else {
+            this.setState({
+                userCheck : {msg: "4-15 characters(no special characters)",
+                            msgClass : "warning",
+                            valid: false
+                }
+            })
+        }
+
+        if(regexEmail.test( this.state.form.email)){
+            this.setState({
+                emailCheck: {msg : "valid email address",
+                            msgClass : "regularMsg",
+                            valid: true
+                }
+            }, this.checkValid);
+        } else {
+            this.setState({
+                emailCheck: {msg : "enter a valid email address",
+                            msgClass : "warning",
+                            valid: false
+                }
+            })
+        }
+
+        if(regexPassword.test( this.state.form.password)){
+            this.setState({
+                passwordCheck: {msg: "valid password",
+                                msgClass : "regularMsg",
+                                valid: true
+                }
+            }, this.checkValid);
+        } else {
+            this.setState({
+                passwordCheck: {msg: "at least 1 letter, 1 number, and at least 8 characters(no special characters)",
+                                msgClass : "warning",
+                                valid: false
+                }
+            })
+        }
+
+        const {value, name} = event.target;
         const{form}= this.state; 
         form[name]= value; 
         this.setState({
@@ -31,23 +97,36 @@ class CreateAccount extends Component {
 
     formatPostData(data){
         const params = new URLSearchParams();
-    
         for(let [k, v] of Object.entries(data)){
             params.append(k, v);
         }
         return params;
     }
 
-    async sendContactForm(context){
-        const { form } = this.state;
-        try {
-            await context.sendNewUsertoServer(form);
+    checkValid(){
+        if(this.state.userCheck.valid && this.state.emailCheck.valid && this.state.passwordCheck.valid){
+            this.setState({
+                submitMsg : ""
+            })
 
-            this.props.history.push('/');
-        } catch (err){
-            console.warn('Did not create a new user');
         }
-        
+    }
+
+    async sendContactForm(context){
+        if(this.state.userCheck.valid && this.state.emailCheck.valid && this.state.passwordCheck.valid) {
+            const {form} = this.state;
+            try {
+                await context.sendNewUsertoServer(form);
+
+                this.props.history.push('/');
+            } catch (err) {
+                console.warn('Did not create a new user');
+            }
+        } else {
+            this.setState({
+                submitMsg : "invalid username or email or password"
+            })
+        }
     }
 
 
@@ -61,16 +140,23 @@ class CreateAccount extends Component {
                <form className='contactForm'> 
                     <div className='contactFormEmail'> 
                          <input placeholder='UserName' type="text" value={username} name='username' onChange={this.handleInputChange}/>
-                    </div> 
+                    </div>
+                    <div className={this.state.userCheck.msgClass}>{this.state.userCheck.msg}</div>
+
                     <div className='contactFormEmail'> 
                         <input placeholder="Email" type="text" value={email} name='email' onChange={this.handleInputChange} /> 
                     </div>
+                    <div className={this.state.emailCheck.msgClass}>{this.state.emailCheck.msg}</div>
+
                     <div className='contactFormEmail'> 
-                        <input placeholder="Password" type="text" value={password} name='password' onChange={this.handleInputChange} /> 
+                        <input placeholder="Password" type="password" value={password} name='password' onChange={this.handleInputChange} />
                     </div>
+                    <div className={this.state.passwordCheck.msgClass}>{this.state.passwordCheck.msg}</div>
+
                     <div className="submitButtonDiv"> 
                     <button className="submitButtonContactPage"  type="button" value="submit" onClick={this.sendContactForm.bind(this,context)}> Submit </button>
-                    </div> 
+                    </div>
+                    <div className="warning">{this.state.submitMsg}</div>
                </form> 
                </div> 
     
